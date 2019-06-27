c0853e59dbe0eb05e821a203db22bd1b316d3819
Stylianos Modes <stelmod@gmail.com>
2019-06-27 22:15:24 +0200

    Implement various functions on List

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -37,32 +37,93 @@ object List { // `List` companion object. Contains functions for creating and wo
       case Cons(h,t) => Cons(h, append(t, a2))
     }
 
-  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
-    as match {
-      case Nil => z
-      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
-    }
-
   def sum2(ns: List[Int]) =
     foldRight(ns, 0)((x,y) => x + y)
 
   def product2(ns: List[Double]) =
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, xs) => xs
+  }
 
-  def tail[A](l: List[A]): List[A] = ???
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => Cons(h, Nil)
+    case Cons(_, xs) => Cons(h, xs)
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  def drop[A](l: List[A], n: Int): List[A] = if (n <= 0) l else drop(tail(l), n - 1)
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(h, xs) => if (f(h)) dropWhile(xs, f) else l
+  }
 
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, Nil) => Nil
+    case Cons(h, xs) => Cons(h, init(xs))
+  }
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int = foldRight(l, 0) ((_: A, z: Int) => z + 1)
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = ???
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(h, xs) => foldLeft(xs, f(z, h)) (f)
+  }
+
+  def appendViaFoldRight[A](l: List[A], r: List[A]): List[A] = foldRight(l, r) ((a, b) => Cons(a, b))
+  def appendViaFoldLeft[A](l: List[A], r: List[A]): List[A] = foldLeft(reverse(l), r) ((b, a) => Cons(a, b))
+
+  def sumLeft(ns: List[Int]): Int = foldLeft(ns, 0) (_ + _)
+  def productLeft(ns: List[Double]): Double = foldLeft(ns, 1.0) (_ * _)
+
+  def reverse[A](l: List[A]): List[A] = foldLeft(l, List[A]()) ((acc: List[A], z: A) => Cons(z, acc))
+
+  def map[A,B](l: List[A])(f: A => B): List[B] = l match {
+    case Nil => Nil
+    case Cons(h, xs) => Cons(f(h), map(xs)(f))
+  }
+
+  def filter[A](l: List[A])(f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(h, xs) => if (f(h)) Cons(h, filter(xs)(f)) else filter(xs)(f)
+  }
+}
+
+object TestList {
+
+  import List._
+
+  // test implementation of `fib`
+  def main(args: Array[String]): Unit = {
+    println("x = " + x)
+    println(tail(List(1, 2, 3, 4)))
+    println(setHead(List(1, 2, 3, 4), -1))
+    println(drop(List(11, 12, 13, 14), 3))
+    println(dropWhile(List(11, 22, 33, 44, 12), (y: Int) => y < 30))
+    println(init(List(11, 12, 13, 14)))
+    println(length(List(11, 12, 13, 14, 15)))
+    val l = List(1, 2, 3, 5)
+    val l2 = List(11, 12, 13, 15)
+    val ld = List(1.0, 2.0, 3.0, 5.0)
+    println(foldLeft(l, 0) (_ + _))
+
+    println(sum2(l) + " " + product2(ld))
+    println(sumLeft(l) + " " + productLeft(ld))
+    println(reverse(l))
+    println(appendViaFoldRight(l, l2))
+    println(appendViaFoldLeft(l, l2))
+    println(map(l) (_ + 1))
+    println(filter(l) (_ % 2 == 0))
+
+  }
 }
