dbaecf0d4a8d56c203ac4c44262b3f3cbfff75ac
Stylianos Modes <stelmod@gmail.com>
2019-07-02 21:56:46 +0200

    Implementation of various functions on Option

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -1,7 +1,7 @@
 package fpinscala.errorhandling
 
 
-import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
+import scala.{Either => _, Option => _, Some => _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
   def map[B](f: A => B): Option[B] = this match {
@@ -9,13 +9,16 @@ sealed trait Option[+A] {
     case Some(v) => Some(f(v))
   }
 
-  def getOrElse[B>:A](default: => B): B = ???
+  def getOrElse[B>:A](default: => B): B = this match {
+    case None => default
+    case Some(v) => v
+  }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
+  def flatMap[B](f: A => Option[B]): Option[B] = map(f) getOrElse None
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = ???
+  def orElse[B>:A](ob: => Option[B]): Option[B] = map(Some(_)) getOrElse ob
 
-  def filter(f: A => Boolean): Option[A] = ???
+  def filter(f: A => Boolean): Option[A] = flatMap( (v: A) => if (f(v)) Some(v) else None)
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
@@ -41,9 +44,13 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = ???
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  def variance(xs: Seq[Double]): Option[Double] = Option.mean(xs).flatMap(m => Option.mean(xs.map(x => math.pow(x - m, 2))))
+
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = (a, b) match {
+    case (Some(a), Some(b)) => Some(f(a, b))
+    case _ => None
+  }
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
 
--- exercises/src/test/scala/fpinscala/errorhandling/OptionTest.scala
+++ exercises/src/test/scala/fpinscala/errorhandling/OptionTest.scala
@@ -12,4 +12,82 @@ class OptionTest extends FunSpec with Matchers {
       Some(5).map(_ * 2) shouldBe Some(10)
     }
   }
+
+  describe("Option getOrElse") {
+    it ("should return default for None") {
+      None.getOrElse(4) shouldBe 4
+    }
+
+    it ("should return value from Some") {
+      Some(5).getOrElse(4) shouldBe 5
+    }
+  }
+
+  describe("Option flatMap") {
+    it ("should not flatMap None") {
+      None.flatMap(_ => Some(4)) shouldBe None
+    }
+
+    it ("should flatMap Some") {
+      Some(5).flatMap(_ => Some(4)) shouldBe Some(4)
+    }
+  }
+
+  describe("Option orElse") {
+    it ("should return default for None") {
+      None.orElse(Some(4)) shouldBe Some(4)
+    }
+
+    it ("should return value from Some") {
+      Some(5).orElse(Some(4)) shouldBe Some(5)
+    }
+  }
+
+  describe("Option filter") {
+    it ("should not change None") {
+      None.filter(_ => false) shouldBe None
+    }
+
+    it ("should filter Some if it doesn't match") {
+      Some(5).filter(_ > 100) shouldBe None
+    }
+
+    it ("should not changes Some if it matches") {
+      Some(5).filter(_ > 4) shouldBe Some(5)
+    }
+  }
+
+  describe("Seq variance") {
+    it ("should be None for empty list") {
+      Option.variance( Seq() ) shouldBe None
+    }
+
+    it ("should be calculated for list with elements") {
+      Option.variance( Seq(3, 3, 9) ) shouldBe Some(8)
+    }
+
+    it ("should be zero for single element list") {
+      Option.variance( Seq(4) ) shouldBe Some(0)
+    }
+  }
+
+  describe("Option map2") {
+    it ("should be None if both None") {
+      Option.map2(None, None)( (a, _) => a) shouldBe None
+    }
+
+    it ("should be None if first None") {
+      Option.map2(None, Some(1)) ( (a, _) => a) shouldBe None
+    }
+
+    it ("should be None if second None") {
+      Option.map2(Some(1), None) ( (a, _) => a) shouldBe None
+    }
+
+    it ("should be calculated if both Some") {
+      Option.map2(Some(1), Some(4)) ( (a, b) => a + b) shouldBe Some(5)
+    }
+
+  }
+
 }
